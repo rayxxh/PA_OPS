04147b8900a4c8b3ff9dda85ae0f20be41e14273
rayxxh <rayxxh@gmail.com>
2016-05-12 11:48:00 +0800

    first upload

---
+++ src/PA_OPS/urls.py
@@ -0,0 +1,41 @@
+"""PA_OPS URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import patterns, url
+from django.contrib import admin
+from PA_OPS.views import *
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', BootstrapView.as_view() ),
+    url(r'^index$',IndexView.as_view(),name='index' ),
+    url(r'^blank$', BlankView.as_view(), name='blank' ),
+    url(r'^buttons', ButtonsView.as_view() , name='buttons'),
+    url(r'^flot', FlotView.as_view(), name='flot'),
+    url(r'^forms$', FormsView.as_view(), name='form' ),
+    url(r'^grid$', GridView.as_view() , name='grid'),
+    url(r'^icons$', IconsView.as_view() , name='icon'),
+    url(r'^login$', LoginView.as_view() , name='login'),
+    url(r'^morris$', MorrisView.as_view(), name='morris' ),
+    url(r'^notifications$', NotificationsView.as_view(), name='notifications' ),
+    url(r'^panels-well$', PanelsWellsView.as_view() , name='panels-wells'),
+    url(r'^tables$', TablesView.as_view() , name='tables'),
+    url(r'^typography$', TypographView.as_view() , name='typography'),
+    
+]
+# urlpatterns = patterns('',
+#     url(r'^admin/', admin.site.urls),
+#     url(r'^$', BootstrapView, name='BootstrapView'), 
+# )
---
+++ src/PA_OPS/views.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.core.files.storage import default_storage
+
+from django.db.models.fields.files import FieldFile
+from django.views.generic.base import TemplateView
+# from django.contrib import messages
+
+
+# http://yuji.wordpress.com/2013/01/30/django-form-field-in-initial-data-requires-a-fieldfile-instance/
+class FakeField(object):
+    storage = default_storage
+
+
+fieldfile = FieldFile(None, FakeField, 'dummy.txt')
+
+
+    
+class BootstrapView(TemplateView):
+    template_name = 'PA_OPS/login.html'
+
+class IndexView(TemplateView):
+    template_name = 'PA_OPS/index.html'
+
+class BlankView(TemplateView):
+    template_name = 'PA_OPS/blank.html'
+   
+class ButtonsView(TemplateView):
+    template_name = 'PA_OPS/buttons.html'
+
+class FlotView(TemplateView):
+    template_name = 'PA_OPS/flot.html'
+
+class FormsView(TemplateView):
+    template_name = 'PA_OPS/forms.html'
+
+class GridView(TemplateView):
+    template_name = 'PA_OPS/grid.html'
+class IconsView(TemplateView):
+    template_name = 'PA_OPS/icons.html'
+class LoginView(TemplateView):
+    template_name = 'PA_OPS/login.html'
+class MorrisView(TemplateView):
+    template_name = 'PA_OPS/morris.html'
+class NotificationsView(TemplateView):
+    template_name = 'PA_OPS/notifications.html'
+class PanelsWellsView(TemplateView):
+    template_name = 'PA_OPS/panels-wells.html'
+class TablesView(TemplateView):
+    template_name = 'PA_OPS/tables.html'
+class TypographView(TemplateView):
+    template_name = 'PA_OPS/typography.html'
